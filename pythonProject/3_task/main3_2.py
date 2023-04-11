629b8fc275f744bed4020ad09f5b7d3a0e51c3c6
Ley_ley_ley1 <liaisansafarova@gmail.com>
2023-04-12 00:26:42 +0300

    added third task

--- pythonProject/3_task/main3_2.py
+++
@@ -1,5 +0,0 @@
-dicts = [{1:[1,2,3,4,5], 2:[6,7,8], 3:[1,3,5,7,9]},{1:[2,3,4], 2:[6,7], 3:[1,3,5]}]
-
-result = { k: set(dicts[0][k]).intersection(*(d[k] for d in dicts[1:])) for k in dicts[0].keys() }
-
-print(result)
